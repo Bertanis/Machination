e21e52b4968a16e471fef100fa76a5cb34d7531a
ResQyuu <albertmandrews@gmail.com>
2021-01-24 14:03:28 -0800

    Added Bootstrap, Materialize, and Font awesome

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ const isDev = process.env.NODE_ENV !== 'production' ? true : false
 const isMac = process.platform === 'darwin' ? true : false
 
 let mainWindow
+let aboutWindow
 
 function createMainWindow() {
     mainWindow = new BrowserWindow({
@@ -16,10 +17,23 @@ function createMainWindow() {
             nodeIntegration: true
         },
         contextIsolation: true,
-        resizable: isDev ? true : false
+        resizable: isDev ? true : false,
+        backgroundColor: 'white'
     })
 
     mainWindow.loadFile('./app/index.html')
+}
+
+function createAboutWindow() {
+    aboutWindow = new BrowserWindow({
+        title: 'About Machination',
+        width: 300,
+        height: 300,
+        resizable: false,
+        backgroundColor: 'white'
+    })
+
+    aboutWindow.loadFile('./app/about.html')
 }
 
 app.on('ready', () => {
@@ -32,16 +46,45 @@ app.on('ready', () => {
 })
 
 const menu = [
-    {
-        label: 'File',
+    ...(isMac ? [{
+        label: app.name,
         submenu: [
             {
-                label: 'Quit',
-                click: () => app.quit()
+                label: 'About',
+                click: createAboutWindow,
             }
         ]
-    }
+    }] : []),
+    {
+        role: 'fileMenu'
+    },
+    ...(!isMac ? [
+        {
+            label: 'Help',
+            submenu: [
+                {
+                    label: 'About',
+                    click: createAboutWindow
+                }
+            ]
+        }
+    ] : []),
+    ...(isDev ? [
+        {
+            label: 'Developer',
+            submenu: [
+                { role: 'reload' },
+                { role: 'forcereload' },
+                { type: 'separator' },
+                { role: 'toggledevtools' }
+            ]
+        }
+    ] : [])
 ]
+
+if (isMac) {
+    menu.unshift({ role: 'appMenu' })
+}
 
 app.on('window-all-closed', () => {
     if (isMac) {
