6d20f7001a0279a2bef8f2eaef423e5d343de0e3
ResQyuu <albertmandrews@gmail.com>
2021-04-27 17:14:30 -0700

    Refactored to layering system

--- app/js/map/map.js
+++ app/js/map/map.js
@@ -1,14 +1,15 @@
 var map = {
     size: 5,
     tsize: 64,
-    tiles: [],
+    layerPriority: ["grid", "token"],
+    layers: {},
     resize(newSize) {
         this.size = newSize + 1;
         $('#map_canvas').attr('width', map.getWidth()).attr('height', map.getHeight());
         this._generateGridTiles();
     },
-    getTile: function (col, row) {
-        return this.tiles[row * map.size + col];
+    getTile: function (name, col, row) {
+        return this.layers[name][row * map.size + col];
     },
     getWidth: function () {
         return this.size * this.tsize;
@@ -16,25 +17,29 @@ var map = {
     getHeight: function () {
         return this.size * this.tsize;
     },
+    setLayerTiles: function (name, tiles) {
+        this.layers[name] = tiles;
+    },
     _generateGridTiles: function () {
-        this.tiles = [];
+        var grid = [];
         for (var r = 0; r < map.size; r++) {
             for (var c = 0; c < map.size; c++) {
                 if (r < map.size - 1) {
                     if (c < map.size - 1) {
-                        this.tiles.push(1);
+                        grid.push(1);
                     } else {
-                        this.tiles.push(2);
+                        grid.push(2);
                     }
                 } else {
                     if (c < map.size - 1) {
-                        this.tiles.push(3);
+                        grid.push(3);
                     } else {
-                        this.tiles.push(0);
+                        grid.push(0);
                     }
                 }
             }
         }
+        this.layers["grid"] = grid;
     }
 };
 map.resize(15);
@@ -46,28 +51,20 @@ Game.load = function () {
 };
 
 Game.init = function () {
-    this.tileAtlas = Loader.getImage('tiles');
+    this.gridAtlas = Loader.getImage('tiles');
 };
 
 Game.update = function (delta) {
 
 };
 
-Game.drawBaseGrid = function () {
-
-}
-
-Game.drawLayer = function (zInd) {
-
-}
-
-Game.render = function () {
+Game.drawLayer = function (name) {
     for (var c = 0; c < map.size; c++) {
         for (var r = 0; r < map.size; r++) {
-            var tile = map.getTile(c, r);
+            var tile = map.getTile(name, c, r);
             if (tile !== 0) {
                 this.ctx.drawImage(
-                    this.tileAtlas,
+                    this.gridAtlas,
                     (tile - 1) * map.tsize,
                     0,
                     map.tsize,
@@ -79,5 +76,11 @@ Game.render = function () {
                 );
             }
         }
+    }
+}
+
+Game.render = function () {
+    for (var name in map.layers) {
+        this.drawLayer(name);
     }
 }
